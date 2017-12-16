a91272f733cb9269a23a55f17744810a301439a6
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2017-12-16 14:52:25 -0800

    Test commit

--- app/src/main/java/com/example/android/minmaxagent/MainActivity.java
+++ app/src/main/java/com/example/android/minmaxagent/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Test commit
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
