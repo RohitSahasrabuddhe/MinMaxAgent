4766cdc2e9fcadb2585ea9d71d534609a487902a
RohitSahasrabuddhe <rohitbhalchandras@gmail.com>
2017-12-28 12:49:02 -0800

    SeekBar working added textViews to indicate seekbar values

--- app/src/main/java/com/example/android/minmaxagent/SettingsActivity.java
+++ app/src/main/java/com/example/android/minmaxagent/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -14,6 +15,7 @@ public class SettingsActivity extends AppCompatActivity {
     // TODO (7) Persistent preferences - next new game has these default settings
 
     EditText valuePlayerName;
+    TextView tvFruitTypeProgressIndicator, tvGridSizeProgressIndicator;
     SeekBar valueFruitTypes, valueGridSize;
     private int fruitTypeProgress, gridSizeProgress;
 
@@ -25,13 +27,17 @@ public class SettingsActivity extends AppCompatActivity {
         valuePlayerName = findViewById(R.id.valueUserName);
 
         valueFruitTypes = findViewById(R.id.valueFruitType);
+        tvFruitTypeProgressIndicator = findViewById(R.id.fruitTypeValueIndicator);
+
         valueFruitTypes.setProgress(0);
-        valueFruitTypes.setMax(9);
+        valueFruitTypes.setMax(6);
 
         valueFruitTypes.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progressValue, boolean b) {
-                fruitTypeProgress = progressValue;
+                int indicatorValue = progressValue + 4;
+                tvFruitTypeProgressIndicator.setText(""+indicatorValue);
+                fruitTypeProgress = indicatorValue;;
             }
 
             @Override
@@ -46,13 +52,17 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
         valueGridSize = findViewById(R.id.valueGridSize);
+        tvGridSizeProgressIndicator = findViewById(R.id.gridSizeValueIndicator);
+
         valueGridSize.setProgress(0);
-        valueGridSize.setMax(9);
+        valueGridSize.setMax(6);
 
         valueGridSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progressValue, boolean b) {
-                gridSizeProgress = progressValue;
+                int indicatorValue = progressValue + 4;
+                tvGridSizeProgressIndicator.setText(""+indicatorValue);
+                gridSizeProgress = indicatorValue;
             }
 
             @Override
