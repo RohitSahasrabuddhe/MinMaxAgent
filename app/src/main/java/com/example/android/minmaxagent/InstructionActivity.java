df64e52db99c78fbb97eccf3abb58ac34fb5b74b
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2018-01-03 20:42:52 -0800

    Help layout and graphics (no code)

--- app/src/main/java/com/example/android/minmaxagent/InstructionActivity.java
+++ app/src/main/java/com/example/android/minmaxagent/InstructionActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.minmaxagent;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class InstructionActivity extends AppCompatActivity {
 
@@ -10,6 +14,19 @@ public class InstructionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                        .setDefaultFontPath("fonts/ComicRelief.ttf")
+                        .setFontAttrId(R.attr.fontPath)
+                        .build() );
+
         setContentView(R.layout.activity_instruction);
+
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
